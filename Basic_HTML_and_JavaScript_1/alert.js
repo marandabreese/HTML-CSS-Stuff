3a421d30ddf1b1e1fdf689e6d16ebf0b09237780
marandabreese <marandabreese@cox.net>
2022-09-01 11:45:50 -0700

    Basic HTML & Javascript assignments

---
+++ Basic_HTML_and_JavaScript_1/alert.js
@@ -0,0 +1,9 @@
+function Cool_Alert() {
+    alert('Hello! I\'m an alert box!');
+}
+
+function My_First_Function() {
+    var test = "Kiss me, I'm Irish!";
+    var result = test.fontcolor("green");
+    document.getElementById("Irish").innerHTML = result 
+}
